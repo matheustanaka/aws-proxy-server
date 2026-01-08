f9e74ac781c3c026481c4a76e9cc463226cc6c8a
Matheus Tanaka <matheustanakanog@gmail.com>
2026-01-08 08:29:05 -0300

    add network resources output

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -10,3 +10,10 @@ output "aws_security_group_allow_ssh" {
 output "aws_security_group_squid_port" {
   value = aws_security_group.squid_port.id
 }
+
+output "vpc_security_group_ids" {
+  value = [
+    aws_security_group.allow_ssh.id,
+    aws_security_group.squid_port.id
+  ]
+}
